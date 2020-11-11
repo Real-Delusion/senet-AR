304c0eb0ff0d959541a103d0a85dde0eef6032f4
almiab1 <almiab1@epsg.upv.es>
2020-11-11 01:02:04 +0100

    DirectionHandler finished

--- Assets/Scripts/TestDirection.cs
+++ Assets/Scripts/TestDirection.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using HandlerDirection;
 
 public class TestDirection : MonoBehaviour
 {
+
+    public DirectionHandler directionHandler = new DirectionHandler();
+    public Image image;
+    public Text message;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,23 @@ public class TestDirection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int direccion = directionHandler.direccionSlide();
+        switch (direccion)
+        {
+            case 1:
+                image.color = Color.Lerp(image.color, Color.yellow, 10f * Time.deltaTime);
+                message.text = "Derecha";
+                break;
+
+            case -1:
+                image.color = Color.Lerp(image.color, Color.red, 10f * Time.deltaTime);
+                message.text = "Izquierda";
+                break;
+
+            default:
+                image.color = Color.Lerp(image.color, Color.cyan, 10f * Time.deltaTime);
+                message.text = "Parado";
+                break;
+        }
     }
 }

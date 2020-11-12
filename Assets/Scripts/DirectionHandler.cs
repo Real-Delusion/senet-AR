5db2707015713f50a4ff54fdc78d6cff0ec271c7
almiab1 <almiab1@epsg.upv.es>
2020-11-12 22:44:10 +0100

    updated directonHandler

--- Assets/Scripts/DirectionHandler.cs
+++ Assets/Scripts/DirectionHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace HandlerDirection
 {
@@ -10,12 +11,13 @@ namespace HandlerDirection
         // Variables de control de la posición
         private Vector2 startPos;
         private Vector2 endingPos;
+        private float diferencia;
 
-        // Variable de la dirección del slide 
+        // Variable de la dirección del slide
         private int direccion;
 
-        // Predefinidos (1 - Derecha) (-1 - Izquierda) (0 - No se ha movido)
-        private static readonly int[] sentido = new int[] { -1, 0, 1 };
+        // Var fija (1 - Derecha) (-1 - Izquierda) (0 - null)
+        public readonly int[] sentido = new int[] { -1 , 0 , 1 };
 
         // Update is called once per frame
         void Update()
@@ -46,20 +48,44 @@ namespace HandlerDirection
 
         public int direccionSlide()
         {
-            if (startPos.magnitude > endingPos.magnitude)
+            diferencia = startPos.x - endingPos.x;
+            if (diferencia > 180 || diferencia < -180)
             {
-                direccion = sentido[2];
-            }
-            else if (startPos.magnitude < endingPos.magnitude)
-            {
-                direccion = sentido[0];
+                if (diferencia > 0f)
+                {
+                    direccion = sentido[2]; // derecha
+                }
+                else
+                {
+                    direccion = sentido[0]; // izquierda
+                }
             }
             else
             {
-                direccion = sentido[1];
+                direccion = sentido[1]; // nada
             }
 
             return direccion;
+        }
+
+        public void testing(Text text)
+        {
+            Debug.Log("Start pos: ( " + startPos.x + " ) - End pos: ( " + endingPos.x + " )");
+            Debug.Log("Diference: "+ diferencia);
+
+            if (direccionSlide() == 1)
+            {
+                text.text = "Start pos: ( " + startPos.x + " ) - End pos: ( " + endingPos.x + " ) - Diference : " + diferencia + "derecha ";
+            }
+            else if (direccionSlide() == -1)
+            {
+                text.text = "Start pos: ( " + startPos.x + " ) - End pos: ( " + endingPos.x + " ) - Diference : " + diferencia + "izquierda ";
+            }
+            else
+            {
+                text.text = "Start pos: ( " + startPos.x + " ) - End pos: ( " + endingPos.x + " ) - Diference : " + diferencia;
+            }
+
         }
     }
 
--- Assets/Scripts/TestDirection.cs
+++ Assets/Scripts/TestDirection.cs
@@ -11,31 +11,27 @@ public class TestDirection : MonoBehaviour
     public Image image;
     public Text message;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         int direccion = directionHandler.direccionSlide();
+        directionHandler.testing(message);
+
         switch (direccion)
         {
             case 1:
                 image.color = Color.Lerp(image.color, Color.yellow, 10f * Time.deltaTime);
-                message.text = "Derecha";
+                //message.text = "Derecha";
                 break;
 
             case -1:
                 image.color = Color.Lerp(image.color, Color.red, 10f * Time.deltaTime);
-                message.text = "Izquierda";
+                //message.text = "Izquierda";
                 break;
 
             default:
                 image.color = Color.Lerp(image.color, Color.cyan, 10f * Time.deltaTime);
-                message.text = "Parado";
+                //message.text = "Parado";
                 break;
         }
     }

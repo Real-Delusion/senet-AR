5f98965c3a4cbf9c72777bc1faa020d775fd17c4
Santiago <santiperez1199@gmail.com>
2020-11-15 17:50:14 +0100

    preventing animation to crash

--- Assets/Scripts/HUDControl.cs
+++ Assets/Scripts/HUDControl.cs
@@ -10,7 +10,6 @@ public class HUDControl : MonoBehaviour
     private GameObject button2;
     private GameObject button3;
     private GameObject button4;
-    private Sprite image;
 
     public GameObject blur;
     public Sprite open;
@@ -22,7 +21,7 @@ public class HUDControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        // Getting the buttons from the parent button (this)
         button1 = this.transform.GetChild(0).gameObject;
         button2 = this.transform.GetChild(1).gameObject;
         button3 = this.transform.GetChild(2).gameObject;
@@ -38,17 +37,25 @@ public class HUDControl : MonoBehaviour
 
     public void OnClick()
     {
-        if (isClicked == false)
+        // LeanTween.isTweening() knows when the animation is running,
+        // !LeanTween.isTweening() is preventing to crash the animation
+        if (isClicked == false && !LeanTween.isTweening())
         {
+            // Change Menu Open sprite to Menu Close
             this.GetComponent<Image>().sprite = close;
+            // Nice open fan animation 
             OpenButtons();
+            // Enable background blur
             blur.SetActive(true);
         }
 
-        else
+        else if(!LeanTween.isTweening())
         {
+            // Change Menu Close sprite to Menu Open
             this.GetComponent<Image>().sprite = open;
+            // Nice close fan animation 
             CloseButtons();
+            // Disable background blur
             blur.SetActive(false);
 
         }
@@ -58,12 +65,10 @@ public class HUDControl : MonoBehaviour
     private void OpenButtons()
     {
 
-        /*LeanTween.move(button1, button1.transform.position + new Vector3(0f, -120f, 0f), 0.3f).setEase(LeanTweenType.easeInQuad);
-        LeanTween.move(button2, button2.transform.position + new Vector3(0f, -240f, 0f), 0.3f).setEase(LeanTweenType.easeInQuad);
-        LeanTween.move(button3, button3.transform.position + new Vector3(0f, -360f, 0f), 0.3f).setEase(LeanTweenType.easeInQuad);
-        LeanTween.move(button4, button4.transform.position + new Vector3(0f, -480f, 0f), 0.3f).setEase(LeanTweenType.easeInQuad);*/
         enableButtons();
 
+        // Scalling the buttons from scale 0.01,0.01,0.01 to 1,1,1 
+        // Adding delay in order to make fan effect
         LeanTween.scale(button1.GetComponent<RectTransform>(), button1.GetComponent<RectTransform>().localScale * 100f, 0.2f);
         LeanTween.scale(button2.GetComponent<RectTransform>(), button2.GetComponent<RectTransform>().localScale * 100f, 0.2f).setDelay(0.1f);
         LeanTween.scale(button3.GetComponent<RectTransform>(), button3.GetComponent<RectTransform>().localScale * 100f, 0.2f).setDelay(0.2f); 
@@ -75,19 +80,16 @@ public class HUDControl : MonoBehaviour
     private void CloseButtons()
     {
 
-        /*LeanTween.move(button1, button1.transform.position + new Vector3(0f, 120f, 0f), 0.3f).setEase(LeanTweenType.easeInQuad);
-        LeanTween.move(button2, button2.transform.position + new Vector3(0f, 240f, 0f), 0.3f).setEase(LeanTweenType.easeInQuad);
-        LeanTween.move(button3, button3.transform.position + new Vector3(0f, 360f, 0f), 0.3f).setEase(LeanTweenType.easeInQuad);
-        LeanTween.move(button4, button4.transform.position + new Vector3(0f, 480f, 0f), 0.3f).setEase(LeanTweenType.easeInQuad);*/
-
+        // Scalling the buttons from scale 1,1,1 to 0.01,0.01,0.01
+        // Adding delay in order to make fan effect
         LeanTween.scale(button4.GetComponent<RectTransform>(), button4.GetComponent<RectTransform>().localScale * 0.01f, 0.2f);
         button4.SetActive(false);
 
         LeanTween.scale(button3.GetComponent<RectTransform>(), button3.GetComponent<RectTransform>().localScale * 0.01f, 0.2f).setDelay(0.1f);
-
         LeanTween.scale(button2.GetComponent<RectTransform>(), button2.GetComponent<RectTransform>().localScale * 0.01f, 0.2f).setDelay(0.2f);
         LeanTween.scale(button1.GetComponent<RectTransform>(), button1.GetComponent<RectTransform>().localScale * 0.01f, 0.2f).setDelay(0.3f);
 
+        // Delaying 0.4f disableButtons method in order to let the animation finishes
         LeanTween.delayedCall(0.4f, disableButtons);
 
 
@@ -109,7 +111,6 @@ public class HUDControl : MonoBehaviour
         button1.SetActive(false);
         button2.SetActive(false);
         button3.SetActive(false);
-        button4.SetActive(false);
 
         isClicked = false;
 

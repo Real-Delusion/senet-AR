b4002a52a8e2387967df6991d41dc38073ba1a90
Rosa <emiliarosavdh@gmail.com>
2020-12-01 20:31:12 +0100

    Change minigame camera script name

--- Assets/_Senet/Scripts/MinigameCamera.cs
+++ Assets/_Senet/Scripts/MinigameCamera.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class MinigameCamera : MonoBehaviour {
-
+public class minigameCamera : MonoBehaviour {
+    // Start is called before the first frame update
     void Start () {
         VuforiaUnity.SetHint (VuforiaUnity.VuforiaHint.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
     }
 
     // Update is called once per frame
-    void Update () { }
+    void Update () {
+    }
 }

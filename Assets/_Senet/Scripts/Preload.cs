7435aba2852d4236cde9ea721b827d1925d77420
almiab1 <almiab1@epsg.upv.es>
2020-12-04 17:05:36 +0100

    preload and load system and game manager added

---
+++ Assets/_Senet/Scripts/Preload.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Preload : MonoBehaviour
+{
+    // UI
+    public GameObject loadingScreen;
+    public Slider slider;
+
+    LevelLoader levelLoader;
+    private void Start()
+    {
+        levelLoader = gameObject.GetComponent<LevelLoader>();
+        LevelLoaderAsy();
+    }
+    void Update()
+    {
+        // Press the space key to start coroutine
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Use a coroutine to load the Scene in the background
+        }
+    }
+
+    private void LevelLoaderAsy()
+    {
+        StartCoroutine(LoadYourAsyncScene("Intro Scene"));
+    }
+
+    IEnumerator LoadYourAsyncScene(string idScene)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(idScene);
+
+        loadingScreen.SetActive(true);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            // UI management
+            float progress = Mathf.Clamp01(asyncLoad.progress / .9f);
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+}

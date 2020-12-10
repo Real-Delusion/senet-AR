cd9cb206337734920bc549d959199bf62c1bd839
Joan Ciprià <joancipria@gmail.com>
2020-12-10 21:59:33 +0100

    Fix pieces position

---
+++ Assets/fixPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fixPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      this.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+}

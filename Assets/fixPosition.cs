9328e8350286cac7218c29ce3f25500674c3a77f
Joan Ciprià <joancipria@gmail.com>
2020-12-10 22:01:25 +0100

    Fix pieces rotation

--- Assets/fixPosition.cs
+++ Assets/fixPosition.cs
@@ -8,11 +8,13 @@ public class fixPosition : MonoBehaviour
     void Start()
     {
         this.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        this.transform.localRotation = Quaternion.identity;
     }
 
     // Update is called once per frame
     void Update()
     {
       this.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        this.transform.localRotation = Quaternion.identity;
     }
 }
